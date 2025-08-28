2c259aceee5891a972c04eab90936698fcdfebc2
Tona <tona.chheun@akilabs.co>
2025-08-28 15:27:22 +0700

    add

--- src/main/java/com/example/spring_graphql_demo/controller/BookController.java
+++ src/main/java/com/example/spring_graphql_demo/controller/BookController.java
@@ -1,9 +1,11 @@
 package com.example.spring_graphql_demo.controller;
 
+import com.example.spring_graphql_demo.dto.BookInput;
 import com.example.spring_graphql_demo.entity.Book;
 import com.example.spring_graphql_demo.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -27,5 +29,13 @@ public class BookController {
     @QueryMapping
     public Book bookById(@Argument String id) {
         return bookRepository.findById(Long.parseLong(id)).orElse(null);
+    }
+
+    @MutationMapping
+    public Book addBook(@Argument BookInput book) {
+        Book entity = new Book();
+        entity.setTitle(book.getTitle());
+        entity.setAuthor(book.getAuthor());
+        return bookRepository.save(entity);
     }
 }
